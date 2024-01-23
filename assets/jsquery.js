76eecec8dc526275e5e431e32349b5fa27abf4ca
nukte-y <nukteu@gmail.com>
2024-01-23 11:33:39 +0000

    added message and a timerfunction to dissappear the message

--- assets/jsquery.js
+++ assets/jsquery.js
@@ -1,8 +1,8 @@
 $("#currentDay").text(dayjs().format('dddd, MMMM D[th]'));  // current day on the header
 var currentHour=dayjs().hour(); 
 var container=$(".container");
-var hoursOfDay= {9:"9AM", 10:"10AM", 11:"11AM", 12:"12PM", 13:"1PM", 14:"2PM", 15:"3PM", 16:"4PM", 17:"5PM" }
-
+var hoursOfDay= {9:"9AM", 10:"10AM", 11:"11AM", 12:"12PM", 13:"1PM", 14:"2PM", 15:"3PM", 16:"4PM", 17:"5PM" };
+var messageBox = $("<h3>").css({ display: "none", justifyContent: "center"}).text("!!Saved to LocalStorage!!💾").appendTo($(container));
 
 for (var key in hoursOfDay){              // creating timeblocks 
 
@@ -31,8 +31,15 @@ for (var key in hoursOfDay){              // creating timeblocks
     var saveIcon=$("<i>").appendTo(saveButton);
     saveIcon.addClass("bi bi-floppy");
     textInput.text(localStorage.getItem(hoursOfDay[key]))    // get stored texts from localstorage
-        
+    // $(".container").text("saved to localStorage");    
 }  
+
+function messageTimer(){                                     //clear message displaying after 800msec
+    var timer = setInterval(function() {
+            messageBox.css({display: "none"});
+            clearInterval(timer);
+        }, 800) 
+}        
     
     $(".bi").on("click",function(event) {                  // added click event to button class
         
@@ -41,6 +48,7 @@ for (var key in hoursOfDay){              // creating timeblocks
         var inputValue=clickedEvent.find("textarea").val();         
         var hourValue=clickedEvent.find(".hour").text();
         localStorage.setItem(hourValue,inputValue);         //storing to localstorage
-
+        messageBox.css({display:"flex"});   
+        messageTimer();
     })
    
